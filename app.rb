98ea8efb74930590d65189763a4171da135ad653
angelos <angelos.kapsimanis@gmail.com>
2014-11-07 23:50:48 +0100

    Added app.rb

---
+++ app.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'sinatra'
+require 'slim'
+require 'pony'
+
+configure do
+  set :public_folder, 'public'
+end
+
+post '/:customer_id/users' do
+  
+end
+
+post '/:customer_id/products' do
+  
+end
+
